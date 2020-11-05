b9b451a3f10d0c485a836edc4b6bed719b8d5c56
mac <mac@macs-MacBook-Pro.local>
2020-11-05 15:08:34 +0100

    app archi/clean

--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
 import 'regenerator-runtime/runtime'
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import createSagaMiddleware from 'redux-saga'
 import mainReducer from './reducers/mainReducer'
-import rootSaga from './sagas/rootSaga'
 import App from './containers/App'
 
-const sagaMiddleware = createSagaMiddleware()
-const middleware = [sagaMiddleware]
-
-const store = createStore(
-  mainReducer,
-  composeWithDevTools(applyMiddleware(...middleware))
-)
-
-sagaMiddleware.run(rootSaga)
+const store = createStore(mainReducer)
 
 render(
   <Provider store={store}>
