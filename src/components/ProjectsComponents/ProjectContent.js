9f55726eb688ab1bdeb0b113ab222c81bd331c4e
mac <mac@macs-MacBook-Pro.local>
2020-11-07 19:58:59 +0100

    feat: draggable component / droppable container

--- src/components/ProjectsComponents/ProjectContent.js
+++ src/components/ProjectsComponents/ProjectContent.js
@@ -1,7 +1,106 @@
-import * as React from 'react'
+import React from 'react'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { useDrag, useDrop } from 'react-dnd'
+import update from 'immutability-helper'
+
+const ItemTypes = {
+  CARD: 'card'
+}
+
+const Card = ({ id, text, index, moveCard }) => {
+  const style = {
+    padding: '0.5rem 1rem',
+    marginBottom: '.5rem',
+    backgroundColor: 'white',
+    cursor: 'move'
+  }
+  const ref = React.useRef(null)
+  const [, drop] = useDrop({
+    accept: ItemTypes.CARD,
+    hover(item, monitor) {
+      if (!ref.current) {
+        return
+      }
+      const dragIndex = item.index
+      const hoverIndex = index
+      if (dragIndex === hoverIndex) {
+        return
+      }
+      const hoverBoundingRect = ref.current?.getBoundingClientRect()
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+      const clientOffset = monitor.getClientOffset()
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return
+      }
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return
+      }
+      moveCard(dragIndex, hoverIndex)
+      item.index = hoverIndex
+    }
+  })
+  const [{ isDragging }, drag] = useDrag({
+    item: { type: ItemTypes.CARD, id, index },
+    collect: monitor => ({
+      isDragging: monitor.isDragging()
+    })
+  })
+  const opacity = isDragging ? 0 : 1
+  drag(drop(ref))
+  return (
+    <div ref={ref} style={{ ...style, opacity }}>
+      {text}
+    </div>
+  )
+}
 
 const ProjectContent = props => {
-  return <>A</>
+  const [cards, setCards] = React.useState([
+    {
+      id: 1,
+      text: 'CARD 1'
+    },
+    {
+      id: 2,
+      text: 'CARD 2'
+    },
+    {
+      id: 3,
+      text: 'CARD 3'
+    }
+  ])
+  const moveCard = React.useCallback(
+    (dragIndex, hoverIndex) => {
+      const dragCard = cards[dragIndex]
+      setCards(
+        update(cards, {
+          $splice: [
+            [dragIndex, 1],
+            [hoverIndex, 0, dragCard]
+          ]
+        })
+      )
+    },
+    [cards]
+  )
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <div style={{ width: '400px' }}>
+        {cards.map((card, index) => (
+          <Card
+            key={card.id}
+            index={index}
+            id={card.id}
+            text={card.text}
+            moveCard={moveCard}
+          />
+        ))}
+      </div>
+    </DndProvider>
+  )
 }
 
 export default ProjectContent
